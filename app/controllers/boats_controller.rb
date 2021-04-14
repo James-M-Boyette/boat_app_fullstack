05d4238a009ba7ff342e1ec7e12553b3e263643c
James Boyette <james.boyette@gmail.com>
2021-04-13 21:42:31 -0400

    Add Create + redirect

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -8,4 +8,17 @@ class BoatsController < ApplicationController
     @boat = Boat.find_by(id: params[:id])
     render "show.html.erb"
   end
+
+  def create
+    @boat = Boat.new(
+      name: params[:name],
+      price: params[:price],
+      capacity: params[:capacity],
+      model: params[:model],
+    )
+    @boat.save
+
+    # this.$router.push(/boats/${this.$route.params.id})
+    redirect_to "/boats/#{@boat.id}"
+  end
 end
